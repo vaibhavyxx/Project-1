ca9033dcccde3cbf3a19d15023ed7c53c1faac2f
v <vd5443@rit.edu>
2026-02-21 15:47:47 -0500

    the data does not go through response json

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,22 +1,51 @@
 const fs = require('fs');   //file system
-const { books } = require("./htmlResponses")
+const { books } = require("./htmlResponses");
+const { request } = require('http');
 
 const respond = (request, response, status, object)=> {
     const content = JSON.stringify(object);
-    response.writeHead(status, {                    //writes to the header
+    response.writeHead(status, {                    
         'Content-Type': 'application/json',
         'Content-Length': Buffer.byteLength(content, 'utf8'),
     });
-    if(request.method !== 'HEAD' && status !== 204){ //204= No New Content
-        response.write(content); //writes to the body
+    if(request.method !== 'HEAD' && status !== 204){ 
+        response.write(content);
     }
-    response.end();     //signals that the data has been sent
+    response.end();    
 }
 
 const getData = (request, response) =>{
     //read the data from parsed JSON 
     const responseJSON = {books};
     respond(request, response, 200, responseJSON);
+}
+
+const addData = (request, response) => {
+    const jsonMessage = {message: 'Everything is required',};
+    const {author, country, language, title, year, genres} = request.body;
+
+    if(!author || !country || !language || !title || !year || !genres){
+        jsonMessage.id = 'Missing parameters';
+        return respond(request, response, 400, jsonMessage);
+    }
+
+    let responseStatus = 204; //updated
+    if(!books[title]){
+        responseStatus = 201;
+        books[title] ={
+            author: author,
+            country: country,
+            language: language,
+            title: title,
+            year: year,
+            genres: genres,
+        };
+    }
+    if(responseStatus === 201){
+        jsonMessage.message = 'Created successfully';
+        return respond(request, response, responseStatus, jsonMessage);
+    }
+    return respond(request, response, responseStatus, {});
 }
 
 module.exports = {getData};
